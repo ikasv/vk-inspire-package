7a8d2eabcce8e25051b298caecf30b1488d52dd2
Sudhir <sudhir@honeycombindia.net>
2023-08-29 11:07:10 +0530

    auto discovery updates

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Vk\Inspire\Http\Controllers\VkInspireController;
+use Vikassoftechure\Inspire\Http\Controllers\VkInspireController;
 
 Route::group(['namespace' => 'Vikassoftechure\Inspire\Http\Controllers'], function(){
     Route::get('daily-inspire', [VkInspireController::class, 'index'])->name('vk-daily-inspire');
